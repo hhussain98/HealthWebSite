0d9049043467a59988a65255e4fe20a46f536db8
hamza <hamzalv12@gmail.com>
2020-02-12 17:08:13 +0000

    First attempt at authenticating the fitbit read api

--- app.js
+++ app.js
@@ -5,10 +5,49 @@ app.use(express.static("public"));
 //all our templates will be ejs
 app.set("view engine","ejs");
 
+// initialize the Fitbit API client
+const FitbitApiClient = require("fitbit-node");
+const client = new FitbitApiClient({
+    clientId: "22BL9L",
+    clientSecret: "38c1b3e81cc8e69a7e1715a062a79b99",
+    apiVersion: '1.2' // 1.2 is the default
+});
+
 app.get("/", function (req, res) {
     res.render("home");
 });
 
+
+
 app.listen(3000, function () {
     console.log("EHealth is runningnpm");
 });
+
+
+// // redirect the user to the Fitbit authorization page
+// app.get("/authorize", (req, res) => {
+//     // request access to the user's activity, heartrate, location, nutrion, profile, settings, sleep, social, and weight scopes
+//     res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'http://localhost:3000/callback'));
+// });
+//
+// // handle the callback from the Fitbit authorization flow
+// app.get("/callback", (req, res) => {
+//     // exchange the authorization code we just received for an access token
+//     client.getAccessToken(req.query.code, 'http://localhost:3000/callback').then(result => {
+//         // use the access token to fetch the user's profile information
+//         client.get("/activities/date/2020-02-12.json", result.access_token).then(results => {
+//             res.send(results[0]);
+//         }).catch(err => {
+//             res.status(err.status).send(err);
+//         });
+//     }).catch(err => {
+//         res.status(err.status).send(err);
+//     });
+// });
+//
+// app.get("/logout", function(req, res) {
+//     req.session.authorized = false;
+//     req.session.access_token = null;
+//     req.session.save();
+//     res.redirect("/");
+// })
