84f06498082de1076b87d49ec4ce5b9e85b131fc
hamza <hamzalv12@gmail.com>
2020-03-12 20:21:02 +0000

    Added new test aswell new deployment tool

--- test/app.test.js
+++ test/app.test.js
@@ -1,38 +1,38 @@
-const Browser = require('zombie');
-
-// We're going to make requests to http://example.com/signup
-// Which will be routed to our test server localhost:3000
-Browser.localhost('example.com', 3000);
-
-describe('Slider menu hide and unhide ', function () {
-    const browser = new Browser();
-    before(function () {
-        return browser.visit('/');
-    });
-    describe('Submit log in form', function (done) {
-        before(function () {
-            browser.fill('#username', 's')
-                .then(() => browser.fill('password', 's'))
-                .then(() => browser.pressButton('#loginButton'))
-                .then(() => browser.wait(10000).then(function () {
-                    browser.assert.status(200);
-                    browser.assert.text('title', "Patient");
-                }).then(done, done));
-        });
-
-        after(function (done) {
-            it('The user ha logged into the home page ', function () {
-                browser.assert.text('title', "Patient");
-            });
-
-            it('The elements on the slide are visiable ', function () {
-                browser.assert.element('#logoutFitbit');
-            });
-        });
-
-    });
-});
-
-
-
-
+// const Browser = require('zombie');
+//
+// // We're going to make requests to http://example.com/signup
+// // Which will be routed to our test server localhost:3000
+// Browser.localhost('example.com', 3000);
+//
+// describe('Slider menu hides and unhides', function () {
+//     const browser = new Browser();
+//     before(function () {
+//         return browser.visit('/');
+//     });
+//     describe('Submit log in form', function (done) {
+//         before(function () {
+//             browser.fill('#username', 's')
+//                 .then(() => browser.fill('password', 's'))
+//                 .then(() => browser.pressButton('#loginButton'))
+//                 .then(() => browser.wait(10000).then(function () {
+//                     browser.assert.status(200);
+//                     browser.assert.text('title', "Patient");
+//                     browser.assert.element('#updateProfileButton');
+//                 }).then(done, done));
+//         });
+//         it('The user has logged into the GP page ', function () {
+//             browser.assert.success();
+//         });
+//
+//         describe('Hide the Nav bar', function () {
+//             after(function () {
+//                 rowser.pressButton('#menu-toggle')
+//                 then(() => browser.wait(10000).then(function () {
+//                     browser.assert.element('#updateProfilfeButton');
+//                 }).then(done, done));
+//
+//             });
+//         });
+//     });
+//
+// });
